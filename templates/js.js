c71ef473ae5a0901d80d00031d5960fa71218b90
hyeoju <hyeogun3806@naver.com>
2024-01-11 04:47:37 +0900

    ㅇㅇ

--- templates/js.js
+++ templates/js.js
@@ -1,13 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const carousel = document.querySelector('.carousel');
-    let counter = 0;
+var kiosk_link = document.querySelector('show-kiosk');
+var kiosk_content = document.querySelector('.kiosk_hiden');
 
-    setInterval(() => {
-        carousel.style.transform = `translateX(-${counter * 300}px)`; // 300px는 이미지 너비입니다.
-        counter++;
-
-        if (counter === 3) {  // 이미지가 3개라고 가정합니다.
-            counter = 0;
-        }
-    }, 2000);  // 2초마다 이미지 전환
+kiosk_link.addEventListener('click', function(e) {
+  e.preventDefault(); // 링크의 기본 동작을 막음
+  if (kiosk_content.style.display === 'none') {
+    kiosk_content.style.display = 'block'; // kiosk_hiden 클래스를 가진 요소를 보이게 함
+  } else {
+    kiosk_content.style.display = 'none'; // kiosk_hiden 클래스를 가진 요소를 숨김
+  }
 });
+document.querySelector('.nav-item:nth-child(3)').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector('.sent_hiden').style.display = 'block';
+  });
+  
+
+
+
